5ca4ce543f38e490cdd3dd903404d16ce53ebe26
avirgil <avirgil@hotmail.fr>
2020-12-29 17:56:38 +0100

    head fixing

--- resources/views/view-profile.blade.php
+++ resources/views/view-profile.blade.php
@@ -1,8 +1,14 @@
 <x-app-layout> 
     <div class="">
             <div class="row "style="background:white">                         
-                <div class="col-lg-4 shadow" style="">
-                    <img  class="img-responsive" src="../../public/storage/<?php $photo=$data['profile_photo_path']; echo $photo ; ?>"/>  
+                <div class="col-lg-4 shadow" 
+                style="width:100%; 
+                height:300px;
+                background-image: url('../../public/storage/<?php $photo=$data['profile_photo_path']; echo $photo ; ?>'); 
+                background-position: center; 
+                background-repeat: no-repeat; 
+                background-size: 100% 100%;">
+                                        hi
                 </div>
                 <div class="col-lg-4">
                     <h4 style="margin-top:10px">
@@ -18,36 +24,7 @@
                 </div>
                 
             </div>
-    </div><br>
-
-            <p class="center">More suggestion for you</p><br>
-            <div class="container-fluid d-flex flex-row center">
-            <input type="hidden" {{$users=Auth::user()->all()->where('clienttype','!=',Auth::user()->clienttype)->where('id','!=',Auth::user()->id)}}>
-
-                @foreach ($users as $user)
-                
-                            <div class="rounded-circle shadow" 
-                                 style="width:150px ; height:160px; 
-                                        background-image: url('../../public/storage/<?php $photo=$user['profile_photo_path']; echo $photo ; ?>'); 
-                                        background-position: center; background-repeat: no-repeat; 
-                                        background-size: 100% 100%; margin-right:4px;margin-left:4px">
-                                    
-                                        <div class="text-center" 
-                                              style="background-color:rgba(0, 0, 0, 0.5);
-                                                     height: 75px; width:150px; border-radius: 0px 0px 100px 100px;margin-top:84px"><br>
-                                            <a href="{{$user['id']}}" class="shadow " ><span class="" style="color:white">
-                                                <?php 
-                                                    $longString = $user['name'] ;
-                                                    echo substr($longString, 0, 8).', '.$user['old'];
-                                                ?>
-                                            </span><br>
-                                            view</a>
-                                        </div>
-                                        
-                            </div>
-                            
-                        
-                @endforeach  
+    </div>
     </div>  
 
 </x-app-layout>
