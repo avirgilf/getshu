7334468beaa658e82ca1f74955bfe1e9762f32e1
avirgil <avirgil@hotmail.fr>
2020-12-31 06:43:17 +0100

    message styling

--- resources/views/view-profile.blade.php
+++ resources/views/view-profile.blade.php
@@ -1,30 +1,24 @@
 <x-app-layout> 
     <div class="">
             <div class="row "style="background:white">                         
-                <div class="col-lg-4 shadow" 
-                style="width:100%; 
-                height:300px;
-                background-image: url('../../public/storage/<?php $photo=$data['profile_photo_path']; echo $photo ; ?>'); 
-                background-position: center; 
-                background-repeat: no-repeat; 
-                background-size: 100% 100%;">
-                                        hi
+                <div class="col-lg-4 " style="">
+                    <img  class="img-responsive" src="../../public/storage/<?php $photo=$data['profile_photo_path']; echo $photo ;?>"/>  
+                    <div class="bottom-left shadow-lg" style="color:white">
+                        <h4>
+                            <?php 
+                                $longString = $data['name'] ;
+                                echo ucfirst(substr($longString, 0, 8).', '.$data['old']);
+                            ?> 
+                        </h4>
+                    </div>
                 </div>
-                <div class="col-lg-4">
-                    <h4 style="margin-top:10px">
-                        <?php 
-                            $longString = $data['name'] ;
-                            echo ucfirst(substr($longString, 0, 8).', '.$data['old']);
-                        ?>
-                        <a href="../chat/{{$data['id']}}"><i class="fas fa-paper-plane" style="color:black;font-size:25px; margin:auto;"></i></a>  
-                       
-                    </h4>
-                <div class="col-lg-4 shadow-sm">
-                    <h2>Offers</h2>
+                <a class="h4" style="margin-top:11px; margin-left:10px; text-decoration:none; color:black" href="../chat/{{$data['id']}}">Send a message...<i class="fas fa-paper-plane" style="color:black;font-size:25px; margin:auto;"></i></a>  
+
+
+                <div class="col-lg-4 shadow-sm" style="margin-left:10px;">
                 </div>
                 
             </div>
-    </div>
     </div>  
 
 </x-app-layout>
