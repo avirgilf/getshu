d4ac5be82f223ad2d35d2a34e2f48fe0e5800525
avirgil <avirgil@hotmail.fr>
2021-01-07 18:58:28 +0100

    message liste

--- app/Http/Controllers/chatController.php
+++ app/Http/Controllers/chatController.php
@@ -14,7 +14,7 @@ class chatController extends Controller
     //
     public function index(){
         $messages = Message::select(DB::raw('t.*'))
-            ->from(DB::raw('(SELECT * FROM messages ORDER BY created_at DESC) t'))
+            ->from(DB::raw('(SELECT * FROM messages) t'))
             ->where('t.from','=',Auth::user()->id)
             ->groupBy('t.to')
             ->orderBy('t.created_at','DESC')
