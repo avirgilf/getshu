fd77473896e66afd4fdcf9233e82842deeadfc51
avirgil <avirgil@hotmail.fr>
2021-01-07 17:34:26 +0100

    message liste

--- resources/views/conversations.blade.php
+++ resources/views/conversations.blade.php
@@ -1,18 +1,18 @@
 <x-app-layout>
 <br>
             @foreach ($messages as $message)
-            <div class="container" style="border-radius:5px 20px 5px; background-color:white">
+            <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
                 <div class="row">
                     <div class="col-2" style="margin:0px; padding:0px">
                         <img style="border-radius:100%;"  src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>" />
                         
                     </div>
                     <div class="col-10 " style="margin:0px; padding:0px">
-                        <div class="col-sm" style="width:100%;border-radius: 600px 10px;background-color:#EFEFEF; margin-top:11px">
+                        <div class="col-sm" style="width:100%;border-radius: 600px 10px;background-color:#EFEFEF; margin-top:13px">
                             <a style="text-decoration:none; color:black; " href="chat/{{ $message->to }}"> {{ $message->toname }} </a>
                         </div>
                         <div class="col-sm text-right" style="width:100%;border-radius: 10px 600px;margin-bottom:10px; background-color:#FD5068; font-size:15px; heigth:30px">                          
-                            <a style="text-decoration:none; color:white; " href="chat/{{ $message->to }}"> {{ substr($message->content,0,20) }}  .... </a>
+                            <a style="text-decoration:none; color:white; " href="chat/{{ $message->to }}"> {{ substr($message->content,0,50) }}  .... </a>
                         </div>
                     </div>
                     
