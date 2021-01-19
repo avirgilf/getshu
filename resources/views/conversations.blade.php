292cbf7907c835c9af692282a3ea6a265c01ee2e
avirgil <avirgil@hotmail.fr>
2021-01-19 19:33:08 +0100

    ajout des champs

--- resources/views/conversations.blade.php
+++ resources/views/conversations.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
 <br>
+    <?php $user_type=Auth::user()->clienttype; if ($user_type==1) :?>
             @foreach ($messages as $message)
             <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
                 <div class="row">
@@ -20,5 +21,28 @@
                 </div>
             </div>
             @endforeach
+
+        <?php else:  ?>
+            @foreach ($messages as $message)
+            <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
+                <div class="row">
+                    <div class="col-2" style="margin:0px; padding:0px">
+
+                        <img class="block rounded-full w-20 h-20" style="border-radius:100%;"  src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>" />
+                        
+                    </div>
+                    <div class="col-10 " style="margin:0px; padding:0px">
+                        <div class="col-sm" style="width:100%;border-radius: 600px 10px;background-color:#EFEFEF; margin-top:13px">
+                            <a style="text-decoration:none; color:black; " href="chat/{{ $message->from }}"> {{ $message->from }} <span class="badge badge-dark" style="border-radius:50%; float:right">9</span> </a>
+                        </div>
+                        <div class="col-sm text-right" style="width:100%;border-radius: 10px 600px;margin-bottom:10px; background-color:#FD5068; font-size:15px; heigth:30px">                          
+                            <a style="text-decoration:none; color:white; " href="chat/{{ $message->from }}"> {{ substr($message->content,0,30) }}  .... </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            @endforeach
+        <?php endif; ?>
 
 </x-app-layout>
