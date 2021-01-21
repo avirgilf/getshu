184c23cedd99139039e61d02a141d981b25512d6
avirgil <avirgil@hotmail.fr>
2021-01-21 10:38:32 +0100

    search design

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -5,21 +5,11 @@
         <div class="row">
     @if ($users)
         @foreach($users as $user)
-        
-            <div class="col-3" style="margin:0px; padding:0px">
-                <img class="block rounded-full img-responsive" style="border-radius:100%;"  src="./../public/storage/<?php $photo=$user->profile_photo_path; echo $photo ; ?>" />
-                <br>
-                <span class="" style="color:black">
-                        <a href="view-profile/{{$user->id}}" class="shadow" style="color:white; text-decoration:none" >
-                        <?php 
-                            $longString = $user->name ;
-                            echo substr($longString, 0, 8).', '.$user->old;
-                        ?>
-                        </a>
-                    </span>
+
+            <div class="mt-2">
+                    <img src="./../public/storage/<?php $photo=$user->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
             </div>
-            
-        
+                
         @endforeach
     @else
         No result for this search
