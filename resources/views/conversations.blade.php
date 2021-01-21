4df992e17b428c457e54918e953a19addc624477
avirgil <avirgil@hotmail.fr>
2021-01-21 11:48:20 +0100

    search design

--- resources/views/conversations.blade.php
+++ resources/views/conversations.blade.php
@@ -4,23 +4,21 @@
             @foreach ($messages as $message)
             <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
                 <div class="row">
-                <div class="col-3" style="margin:0px; padding:0px">
-                    <div class="" style="margin:0px; padding:0px">
-                      <img src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
-                       
+                <a style="text-decoration:none; color:white; " href="chat/{{ $message->to }}">
+                    <div class="col-3" style="margin:0px; padding:0px">
+                        <div class="" style="margin:0px; padding:0px">
+                            <img src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
+                        </div>
                     </div>
-                </div>
-                
-                   
                     <div class="col-9 " style="margin:0px; padding:0px">
                         <div class="col-sm" style="width:100%;border-radius: 10px 600px;background-color:#EFEFEF; margin-top:16px">
-                            <a style="text-decoration:none; color:black; " href="chat/{{ $message->to }}"> {{ $message->toname }} <span class="badge badge-dark" style="border-radius:50%; float:right">9</span> </a>
+                            {{ $message->toname }} <span class="badge badge-dark" style="border-radius:50%; float:right">9</span> 
                         </div>
                         <div class="col-sm text-right" style="width:100%;border-radius: 600px 10px;margin-bottom:10px; background-color:#FD5068; font-size:15px; heigth:30px">                          
-                            <a style="text-decoration:none; color:white; " href="chat/{{ $message->to }}"> {{ substr($message->content,0,30) }}  .... </a>
+                             {{ substr($message->content,0,30) }}  .... 
                         </div>
                     </div>
-                    
+                </a>
                 </div>
             </div>
             @endforeach
