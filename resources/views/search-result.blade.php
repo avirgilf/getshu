8eecc13d4211be5f3c461e93cbf9bacf30c17954
avirgil <avirgil@hotmail.fr>
2021-01-21 13:02:57 +0100

    search design

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
     @if ($users)
         @foreach($users as $user)
-        <div class="col-3" style="margin:0px;">
+        <div class="col-3" style="margin:5px; padding:0px">
             <div class="mt-2" style="margin:0px; padding:0px">
                 <a href="view-profile/{{$user->id}}" style="color:black; text-decoration:none" >
                     <img src="./../public/storage/<?php $photo=$user->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
