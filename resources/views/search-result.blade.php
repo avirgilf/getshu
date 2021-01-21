51b5611017dd9314dfdb3456dc76240eae3c2962
avirgil <avirgil@hotmail.fr>
2021-01-21 10:58:35 +0100

    search design

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -7,7 +7,13 @@
         @foreach($users as $user)
 
             <div class="mt-2">
+                <a href="view-profile/{{$user->id}}" style="color:black; text-decoration:none" >
                     <img src="./../public/storage/<?php $photo=$user->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
+                        <?php 
+                            $longString = $user->name ;
+                            echo substr($longString, 0, 8).', '.$user->old;
+                        ?>
+                </a>
             </div>
                 
         @endforeach
