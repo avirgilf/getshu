99b985ee0e2f9fc55f6660bb5d502c1f1b53161c
avirgil <avirgil@hotmail.fr>
2021-01-21 11:43:21 +0100

    search design

--- resources/views/conversations.blade.php
+++ resources/views/conversations.blade.php
@@ -13,7 +13,7 @@
                 
                    
                     <div class="col-9 " style="margin:0px; padding:0px">
-                        <div class="col-sm" style="width:100%;border-radius: 10px 600px;background-color:#EFEFEF; margin-top:13px">
+                        <div class="col-sm" style="width:100%;border-radius: 10px 600px;background-color:#EFEFEF; margin-top:15px">
                             <a style="text-decoration:none; color:black; " href="chat/{{ $message->to }}"> {{ $message->toname }} <span class="badge badge-dark" style="border-radius:50%; float:right">9</span> </a>
                         </div>
                         <div class="col-sm text-right" style="width:100%;border-radius: 600px 10px;margin-bottom:10px; background-color:#FD5068; font-size:15px; heigth:30px">                          
