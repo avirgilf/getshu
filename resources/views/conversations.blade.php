3c49b74b6e23e14c7af1b7e2824b54e77db0b2df
avirgil <avirgil@hotmail.fr>
2021-01-21 11:01:46 +0100

    search design

--- resources/views/conversations.blade.php
+++ resources/views/conversations.blade.php
@@ -4,12 +4,12 @@
             @foreach ($messages as $message)
             <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
                 <div class="row">
-                    <div class="col-2" style="margin:0px; padding:0px">
-
-                        <img class="block rounded-full w-20 h-20" style="border-radius:100%;"  src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>" />
-                        
-                    </div>
-                    <div class="col-10 " style="margin:0px; padding:0px">
+                <div class="mt-2">
+                    <img src="./../public/storage/<?php $photo=$user->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
+                       
+                </div>
+                   
+                    <div class="mt-10 " style="margin:0px; padding:0px">
                         <div class="col-sm" style="width:100%;border-radius: 600px 10px;background-color:#EFEFEF; margin-top:13px">
                             <a style="text-decoration:none; color:black; " href="chat/{{ $message->to }}"> {{ $message->toname }} <span class="badge badge-dark" style="border-radius:50%; float:right">9</span> </a>
                         </div>
