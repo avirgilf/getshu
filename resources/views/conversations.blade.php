653f10127fde6c53507e4a4b39339031de5a4aa2
avirgil <avirgil@hotmail.fr>
2021-01-21 11:59:50 +0100

    search design

--- resources/views/conversations.blade.php
+++ resources/views/conversations.blade.php
@@ -4,12 +4,12 @@
             @foreach ($messages as $message)
             <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
                 <div class="row">
-                    <div class="col-3" style="margin:0px; padding:0px">
-                        <div class="" style="margin:0px; padding:0px">
-                            <img src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
-                        </div>
+                <div class="col-3" style="margin:0px; padding:0px">
+                    <div class="" style="margin:0px; padding:0px">
+                      <img src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
+                       
                     </div>
-                
+                </div>
                    
                     <div class="col-9 " style="margin:0px; padding:0px">
                         <div class="col-sm" style="width:100%;border-radius: 10px 600px;background-color:#EFEFEF; margin-top:16px">
@@ -28,12 +28,13 @@
             @foreach ($messages as $message)
             <div class="container" style="border-radius:5px 20px 5px; background-color:white ; margin-bottom:3px">
                 <div class="row">
-                    <div class="col-2" style="margin:0px; padding:0px">
-
-                        <img class="block rounded-full w-20 h-20" style="border-radius:100%;"  src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>" />
-                        
+                <div class="col-3" style="margin:0px; padding:0px">
+                    <div class="" style="margin:0px; padding:0px">
+                      <img src="./../public/storage/<?php $photo=$message->tophoto ; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
+                       
                     </div>
-                    <div class="col-10 " style="margin:0px; padding:0px">
+                </div>
+                <div class="col-9 " style="margin:0px; padding:0px">
                         <div class="col-sm" style="width:100%;border-radius: 600px 10px;background-color:#EFEFEF; margin-top:13px">
                             <a style="text-decoration:none; color:black; " href="chat/{{ $message->from }}"> {{ $message->from }} <span class="badge badge-dark" style="border-radius:50%; float:right">9</span> </a>
                         </div>
