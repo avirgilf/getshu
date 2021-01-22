d3128ed2656631f6b5d5e5f7849b95daf4458db8
avirgil <avirgil@hotmail.fr>
2021-01-22 15:21:23 +0100

    search design

--- resources/views/view-profile.blade.php
+++ resources/views/view-profile.blade.php
@@ -1,25 +1,52 @@
 <x-app-layout> 
-    <div class="">
-            <div class="row "style="background:white"> 
-
-            <div class="col-3" style="margin:0px; padding:0px"> </div>
-            <div class="col-6" style="margin:0px; padding:0px">
-                    <div class="mt-2" style="margin:0px; padding:0px">
-                        <a href="view-profile/{{$data->id}}" style="color:black; text-decoration:none" >
-                            <img src="./../public/storage/<?php $photo=$data->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
-                                <?php 
-                                    $longString = $data->name ;
-                                    echo $longString.' , '.$data->old;
-                                ?>
-                        </a>
+        <div class="container" style=" background-color:white ; margin-top:3px">
+            <div class="row">
+                <div class="col-6">
+                    <div class="" style="margin:0px; padding:0px">
+                            <img src="./../public/storage/<?php $photo=$data->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-30 w-100 object-cover img-responsive shadow">
+                               
                     </div>
-                </div>    
-                <a class="h4" style="margin-top:11px; margin-left:10px; text-decoration:none; color:black" href="../chat/{{$data['id']}}">Send a message...<i class="fas fa-paper-plane" style="color:black;font-size:25px; margin:auto;"></i></a>  
-
-            </div>
-            <div class="col-3" style="margin:0px; padding:0px"> </div>
+                </div>
+                <div class="col-6 text-center" style=" padding:0px; background-color:#FD5068; ">
+                    <div style="margin-top:40%; ">
+                    <a style="text-decoration:none; color:white" href="../chat/{{$data['id']}}">
+                        <i class="fa fa-comments display-4" aria-hidden="true"></i>
+                        <br>Send a message to
+                        <br>
+                        <?php 
+                            $longString = $data->name ;
+                            echo $longString.' , '.$data->old;
+                         ?><br>
+                         <i class="fa fa-map-marker" style="color:green" aria-hidden="true"></i>
+                         {{ $data->countryid .' '. $data->cities_id}}
+                     </a>
+                    <!--<i class="fa fa-phone display-4" aria-hidden="true"></i><br>
+                    <i class="fa fa-video" aria-hidden="true"></i>-->
+                    </div>
+                </div>
+                <div  class="col-12 top-3">
+                    <hr>
+                </div>
+                <div class="col-12 text-center">
+                    <?php 
+                            $description = $data->description ;
+                            echo $description ;
+                            
+                     ?>
+                </div>
+                <div  class="col-12">
+                     <hr>
+                </div>
+                <div class=" col-12 text-center">
+                   <p class="shadow-lg">What do i offer you ?</p>
+                   <i class="fa fa-hand-o-down" aria-hidden="true"></i>
+                </div>
+                <div class="col-12 text-center " style="background-color:white">
+                   <p class="h6 bold" style="">Cash money & Good time</p>
+                </div>
                 
             </div>
+            
     </div>  
 
 </x-app-layout>
