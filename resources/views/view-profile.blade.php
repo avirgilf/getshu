c70df98831bbde14915132e17c43bffd949110ff
avirgil <avirgil@hotmail.fr>
2021-01-22 10:12:15 +0100

    search design

--- resources/views/view-profile.blade.php
+++ resources/views/view-profile.blade.php
@@ -3,10 +3,10 @@
         <div class="row "style="background:white"> 
 
             <div class="col-3" style="margin:0px; padding:0px"> </div>
-            <div class="col-6" style="margin:10px; padding:0px">
+            <div class="col-6" style="margin:0px; padding:0px">
                     <div class="mt-2" style="margin:0px; padding:0px">
-                        <a  style="margin:10px; padding:0px" href="view-profile/{{$data->id}}" style="color:black; text-decoration:none" >
-                            <img style="margin:10px; padding:0px" src="./../public/storage/<?php $photo=$data->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-45 w-45 object-cover">
+                        <a  style="margin:0px; padding:0px" href="view-profile/{{$data->id}}" style="color:black; text-decoration:none" >
+                            <img style="margin:10px; padding:0px" src="./../public/storage/<?php $photo=$data->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-35 w-35 object-cover">
                                 <?php 
                                     $longString = $data->name ;
                                     echo $longString.' , '.$data->old;
