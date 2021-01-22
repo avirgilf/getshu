053168c665a9272c8e07136e55553965e8f3d58d
avirgil <avirgil@hotmail.fr>
2021-01-22 10:04:05 +0100

    search design

--- resources/views/view-profile.blade.php
+++ resources/views/view-profile.blade.php
@@ -1,24 +1,24 @@
 <x-app-layout> 
     <div class="">
-            <div class="row "style="background:white">                         
-                <div class="col-lg-4 " style="">
-                    <img  class="img-responsive" src="../../public/storage/<?php $photo=$data['profile_photo_path']; echo $photo ;?>"/>  
-                    <div class="bottom-left shadow-lg" style="color:white">
-                        <h4>
-                            <?php 
-                                $longString = $data['name'] ;
-                                echo ucfirst(substr($longString, 0, 8).', '.$data['old']);
-                            ?> 
-                        </h4>
+        <div class="row "style="background:white"> 
+
+            <div class="col-3" style="margin:0px; padding:0px"> </div>
+            <div class="col-6" style="margin:10px; padding:0px">
+                    <div class="mt-2" style="margin:0px; padding:0px">
+                        <a href="view-profile/{{$data->id}}" style="color:black; text-decoration:none" >
+                            <img src="./../public/storage/<?php $photo=$data->profile_photo_path; echo $photo ; ?>"  class="rounded-full  object-cover">
+                                <?php 
+                                    $longString = $data->name ;
+                                    echo $longString.' , '.$data->old;
+                                ?>
+                        </a>
                     </div>
-                </div>
+                </div>    
                 <a class="h4" style="margin-top:11px; margin-left:10px; text-decoration:none; color:black" href="../chat/{{$data['id']}}">Send a message...<i class="fas fa-paper-plane" style="color:black;font-size:25px; margin:auto;"></i></a>  
 
-
-                <div class="col-lg-4 shadow-sm" style="margin-left:10px;">
-                </div>
-                
             </div>
+            <div class="col-3" style="margin:0px; padding:0px"> </div>                
+        </div>
     </div>  
 
 </x-app-layout>
