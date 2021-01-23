9e4bbffcc8a2184c15e151a998aeb1ebbda30b95
avirgil <avirgil@hotmail.fr>
2021-01-23 17:32:41 +0100

    change

--- resources/views/view-profile.blade.php
+++ resources/views/view-profile.blade.php
@@ -42,7 +42,7 @@
                    <i class="fa fa-hand-o-down" aria-hidden="true"></i>
                 </div>
                 <div class="col-12 text-center " style="background-color:white">
-                   <p class="h6 bold" style="">Hookup & excort</p>
+                   <p class="h6 bold" style="">Hookup & escort</p>
                 </div>
                 
             </div>
