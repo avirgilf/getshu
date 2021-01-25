32dda2b2a9ad481be051efb024ff633a0fde90a2
avirgil <avirgil@hotmail.fr>
2021-01-25 14:22:13 +0100

    changess

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -80,24 +80,21 @@
                  $newuser=\App\Models\User::All()->where('countryid','==',Auth::User()->countryid);
 
             ?>
-            @if ($users)
-                @foreach($newusers as $user)
+                @foreach($newuser as $user)
                 <div class="col-3" style="margin:10px; padding:0px">
                     <div class="mt-2" style="margin:0px; padding:0px">
                         <a href="view-profile/{{$user->id}}" style="color:black; text-decoration:none" >
                             <img src="./../public/storage/<?php $photo=$user->profile_photo_path; echo $photo ; ?>"  class="rounded-full h-20 w-20 object-cover">
                                 <?php 
                                     $longString = $user->name ;
-                                    echo substr($longString, 0, 8).', '.$user->old;
+                                    echo substr($longString, 0, 8).','.$user->old;
                                 ?>
                         </a>
                     </div>
                 </div>
                         
                 @endforeach
-            @else
-                No result for this search
-            @endif
+            
         </div>
 
         </div>
